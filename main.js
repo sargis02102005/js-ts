99815c6fca1d45d6c8c4224a1526373ad62b73fa
Саргис <oganyan.sargis20@mail.ru>
2025-07-25 15:39:08 +0300

    отрефакторил код

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const text = 'EXAMPLE';
+const text = 'example';
+const start = text.charAt(0).toUpperCase()// E
+const middle = text.slice(1, text.length - 1).toLowerCase()//xampl
+const end = text.charAt(text.length - 1).toUpperCase()//E
+//           E    +  xampl  + E
+console.log(start + middle + end)
 
-const firstChar = text.charAt(0)
-const lastChar = text.charAt(text.length - 1)
-
-
-console.log(firstChar.toUpperCase() + text.slice(1, text.length - 1).toLowerCase() + lastChar.toUpperCase())
