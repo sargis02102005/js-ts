a2d3670c51dafb661166447cf4a3fd11126526dd
Саргис <oganyan.sargis20@mail.ru>
2025-07-25 16:00:12 +0300

    измение кода

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-const text = 'example';
-const start = text.charAt(0).toUpperCase()// E
-const middle = text.slice(1, text.length - 1).toLowerCase()//xampl
-const end = text.charAt(text.length - 1).toUpperCase()//E
-//           E    +  xampl  + E
-console.log(start + middle + end)
-
+// const text = 'молоко';
+// const start = text.charAt(0).toUpperCase()// E
+// const middle = text.slice(1, text.length - 1).toLowerCase()//xampl
+// const end = text.charAt(text.length - 1).toUpperCase()//E
+// //           E    +  xampl  + E
+// console.log(start + middle + end)
+//Поиск позиции символа
+// console.log(text.indexOf('о')) // Позиции 1
+// console.log(text.lastIndexOf('о'))// Позиции 5
+//Поиск вхождения символа
+// console.log(text.includes('у'), ) //false
+// console.log(text.includes('м'), ) //true
