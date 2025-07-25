2c3bbabd754156d997b4d3a2fa5e65d3ce7db52b
Саргис <oganyan.sargis20@mail.ru>
2025-07-25 15:24:11 +0300

    Сделал первый и последний символ строки

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const text = 'example';
-// Сделал страку в заглавном регистре
-console.log(text.toUpperCase())
+const text = 'EXAMPLE';
 
+const firstChar = text.charAt(0)
+const lastChar = text.charAt(text.length - 1)
+
+
+console.log(firstChar.toUpperCase() + text.slice(1, text.length - 1).toLowerCase() + lastChar.toUpperCase())
