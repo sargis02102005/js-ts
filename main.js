062f70ab4f5271e2f435615ef0e5c73d63eb6ca2
Саргис <oganyan.sargis20@mail.ru>
2025-07-25 16:33:46 +0300

    площадь круга

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-let a = 4;
-let area = a * a; //
-console.log("Площадь квадрата со стороной a=" + a + " равна " + area); // Вывод: Площадь квадрата со стороной a=4 равна 16
+// 1. Генерируем радиус круга (целое число от 1 до 20)
+const radius = Math.floor(Math.random() * 20) + 1;
 
+// 2. Вычисляем площадь круга
+const area = Math.PI * radius * radius;
+
+// 3. Округляем площадь до 2 знаков после запятой
+const roundedArea = area.toFixed(2); // toFixed() возвращает строку!
+
+// 4. Формируем сообщение
+const message = `Радиус: ${radius}\nПлощадь: ${roundedArea}`;
+
+// 5. Выводим сообщение
+console.log(message);
 
 
