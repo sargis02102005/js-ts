85a3e03f58a08c236552ec5a3ba984850b2a120c
Саргис <oganyan.sargis20@mail.ru>
2025-07-25 16:04:55 +0300

    разделитель страки

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-// const text = 'молоко';
-// const start = text.charAt(0).toUpperCase()// E
-// const middle = text.slice(1, text.length - 1).toLowerCase()//xampl
-// const end = text.charAt(text.length - 1).toUpperCase()//E
-// //           E    +  xampl  + E
-// console.log(start + middle + end)
-//Поиск позиции символа
-// console.log(text.indexOf('о')) // Позиции 1
-// console.log(text.lastIndexOf('о'))// Позиции 5
-//Поиск вхождения символа
-// console.log(text.includes('у'), ) //false
-// console.log(text.includes('м'), ) //true
+const text = 'this_is_an_example_string';
+// Разделитель
+console.log(text.split('_'))
