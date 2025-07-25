450ad13f999f25dea4e24304e65d8bce2a0fd8c7
Саргис <oganyan.sargis20@mail.ru>
2025-07-25 15:01:35 +0300

    Сделал страку в заглавном регистре

--- main.js
+++ main.js
@@ -1,40 +1,4 @@
-// let name1 = 'Anton';
-// let name2 = name1;
-// name2 = 'Alex';
-//
-// name1 = 'Kirill';
-// name2 = name1;
-//
-// name1 = 'Evgeniy';
-// Значение переменных name1 и name2 = Evgeniy Kirill
-// console.log(name1, name2)
-
-
-// тут ошибка в let const, а так же переменная не имеет имени
-// const aaa = 'letter';
-// ошибка в  const -можно присвоить значение только 1 раз
-// let maxAge = 100;
-// maxAge = 99;
-//
-// let myName = "Ruslan";
-// myName = '';
-//
-// let yourName = `Mikhail`;
-// yourName = "";
-// ошибка в  const -можно присвоить значение только 1 раз
-// let PI = 3.14;
-//
-// let hisName = 'Vitaliy';
-// hisName = 10;
-//
-// let _ = 'some info';
-// _ = 10;
-
-// let a = 10;
-// let b = 20;
-//
-// c = b
-//
-//
-// console.log(c, a); // 20, 10
+const text = 'example';
+// Сделал страку в заглавном регистре
+console.log(text.toUpperCase())
 
