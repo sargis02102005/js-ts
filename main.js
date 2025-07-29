4cba91bba696b712a3dbd6399366fed2f7121bbc
Саргис <oganyan.sargis20@mail.ru>
2025-07-29 17:57:17 +0300

    условия:проверка кода

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-const a = 2 - Math.round(Math.random() * 4 )
-/*
-// Без сокращенния
-if (a > 0){
-    console.log('положительное')
-}
-else if (a === 0){
-    console.log('null')
-}
-else {
-    console.log('отрицательное')
-}
-*/
-// Сокращённый вариант
-/*const massenger =
-    a > 0 ? 'положительное' : a < 0 ? 'отрицательное' : 'null';
+const age = Math.round(Math.random() * 90);
 
-console.log(massenger);
-*/
-// Сокращённый вариант №2
-console.log(a > 0 ? 'положительное' : a < 0 ? 'отрицательное' : 'null')
+console.log(`${age >= 18 ? '' : 'не'}совершеннолетний`)
+// Выводит совершеннолетний и несовершеннолетний с соответсвием возроста
