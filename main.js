f21da9368d3ba6ee922180c5f35f8335cf8b296b
Саргис <oganyan.sargis20@mail.ru>
2025-07-29 17:19:40 +0300

    условия:мой ответ!

--- main.js
+++ main.js
@@ -1,26 +1,76 @@
-let a = 10;
+const a = 10;
 
-console.log(1 > 0);//true
-console.log(!(1 < 0));//true
+if (a === 2 || 3) {
+    console.log(-1);
+ }// -1
 
-console.log('a' == a);//false
-console.log('a' !== a);//true
+if (a === 2 || a === 3) {
+    console.log(0);
+} // false
 
-a = 4;
+if (10 % 1) {
+    console.log(1);
+}// 1
 
-console.log(a == 10);//false
-console.log(a == '10');//false
+if (10 % 0) {
+    console.log(2);
+} else if (10) {
+    console.log(3);
+} else if (true) {
+    console.log(4);
+}// 4
 
-console.log(a >= 4);//true
-console.log('4' == a);//true
+let g = '';
 
-console.log(!(1 === '1'));//true
+if (g) {
+    console.log(5);
+} else {
+    console.log(6);
+}// 6
 
-console.log(!!true);//true
-console.log(!!!('0' == 0));//false
-console.log(false == !false);//false
+if (undefined) {
+    console.log(7);
+} // false
 
-console.log(1 != '1');//false
-console.log(1 !== '1');//true
+if ('0') {
+    console.log(8);
+} else if ('') {
+    console.log(9);
+} else {
+    console.log('a');
+}// 8
 
-console.log(1 != '0');//true
+if (!'0') {
+    console.log('b');
+} else {
+    console.log('c');
+}// c
+
+if (!!'' || !!0 || !!1) {
+    console.log('d');
+}// d
+
+if (undefined || false || '' || 0) {
+    console.log('e');
+} else if (!false && (0 || '0')) {
+    console.log('f');
+} else {
+    console.log('g');
+}// f
+
+const a = 0;
+const b = 1;
+let c;
+
+console.log(a == '0' ? 'h' : 'i');// h
+console.log(!(a > b) ? 'j' : 'k');// j
+console.log(0 ? 'l' : 'm');// m
+console.log(!0 ? 'n' : 'o');// n
+console.log((null === undefined || 11 % 5) && (a > b || !!true) ? 'n' : 'o');// n
+
+console.log('abc' == 'ABC' ? 'p' : 'q');// q
+console.log('false' ? 'r' : 's');// r
+console.log('false' == false ? 't' : 'u');// u
+console.log(Number('one') ? 'v' : 'w');// w
+console.log(c ? 'x' : 'y');// y
+console.log(null == undefined ? 'z' : '🫡');// z
