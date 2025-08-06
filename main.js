b6e53b16ae635f24c2e9c048fcb67faab8b2cadc
Саргис <oganyan.sargis20@mail.ru>
2025-08-06 16:41:29 +0300

    Объекты:Возраст

--- main.js
+++ main.js
@@ -1,31 +1,37 @@
-const user = {
-    id: 40,
-    age: 18,
-    name: "Валентин Петрович",
-    city: "Ковров",
-    address: "ул. олытва  1 корпус 62346283764273468792364 строение Д",
+const alex = {
+    name: 'Alex',
+    dateOfBirth: new Date('2024-05-15'),
 };
 
-if (16 <= user.age && user.age <= 17 && user.city === 'Москва') {
-    // <-- Соверш
-    if (user.city === "Москва") {
-        // <-- Москва
-        if (user.address.includes("Ленина")) {
-            user.address = "ул.Кривая 109"; // Соверш + москва
-        } else {
-            // <-- Ленина
-            user.city = "Иваново"; // Соверш + москва + ленина
-            user.address = "ул.Важная 5"; //
-        }
-    } else {
-        // Если ул капиталистов => материалистов москва
-        // Иначе => ул Центральная
-        if (user.address.includes("Капиталистов")) {
-            user.city = "Москва";
-            user.address = "ул.Материалистов 3";
-        } else {
-            user.address = "ул.Центральная 2";
-        }
-    }
-}
-console.log(user);
+const jake = {
+    name: 'Jake',
+    dateOfBirth: new Date('1970-01-01T00:00:00.000Z'),
+};
+
+const john = {
+    name: 'John',
+    dateOfBirth: new Date('2007-12-30T11:32:59.953+03:00'),
+};
+
+const bob = {
+    name: 'Bob',
+    dateOfBirth: new Date(),
+};
+
+// Дней в году * часов в дне * минут в часе * секунд в часе * миллисекунд в секунде
+const millisecondsInYear = 365 * 24 * 60 * 60 * 1000; // Или сразу просто 31536000000. Кол-во миллисекунд в году.
+
+// Текущая дата
+const now = new Date();
+
+// Делим "сколько прошло миллисекунд с момента рождения" на количество миллисекунд в годе, тем самым находим
+// сколько прошло лет с момента рождения. И округляем вниз, чтобы получить "полных лет"
+alex.age = Math.floor((now.getTime() - alex.dateOfBirth.getTime()) / millisecondsInYear);
+jake.age = Math.floor((now.getTime() - jake.dateOfBirth.getTime()) / millisecondsInYear);
+john.age = Math.floor((now.getTime() - john.dateOfBirth.getTime()) / millisecondsInYear);
+bob.age = Math.floor((now.getTime() - bob.dateOfBirth.getTime()) / millisecondsInYear);
+
+console.log(alex);
+console.log(jake);
+console.log(john);
+console.log(bob);
