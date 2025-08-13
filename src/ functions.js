975d68060297cc58a59a89a40bdd683fa034285c
Саргис <oganyan.sargis20@mail.ru>
2025-08-13 15:31:24 +0300

    Массивы:История никнеймов

--- src/ functions.js
+++ src/ functions.js
@@ -1,26 +1,12 @@
-import chalk from 'chalk';
+const setNickname = (user, newNickname) => {
+  // Добавляем текущий никнейм в историю, если он существует
+  if (user.nickname) {
+    user.nicknameHistory = user.nicknameHistory || [];
+    user.nicknameHistory.push(user.nickname);
+  }
 
-// 1. Удаление поля из объекта
-const removeProperty = (obj, property) => {
-  delete obj[property];
+  // Устанавливаем новый никнейм
+  user.nickname = newNickname;
 };
 
-// 2. Добавление/изменение поля в объекте
-const addProperty = (obj, property, value) => {
-  obj[property] = value;
-};
-
-// 3. Сравнение значений полей двух объектов
-const compare = (obj1, obj2, property) => {
-  const value1 = obj1[property];
-  const value2 = obj2[property];
-  const areEqual = value1 === value2;
-
-  const message = `Сравнение поля "${property}": ${value1} vs ${value2}`;
-  const color = areEqual ? chalk.green.bold : chalk.red.bold;
-
-  console.log(color(message));
-  return areEqual;
-};
-
-export { removeProperty, addProperty, compare };
+export { setNickname };
