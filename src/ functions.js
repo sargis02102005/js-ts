2e9f4b5e860ad55ff9e1fe6693abe5c006871f3b
Саргис <oganyan.sargis20@mail.ru>
2025-08-15 17:28:24 +0300

    Функции:Простые функции

--- src/ functions.js
+++ src/ functions.js
@@ -1,23 +1,28 @@
 import chalk from 'chalk';
 
-// 1. Функция sayHi с chalk
-const sayHi = (name) => {
-  console.log(chalk.green('Привет,') + ' ' + chalk.blue(name));
+// 1. Функция sayHi (Привет, Анатолий), слово “Привет” зелёное, а имя пользователя синие
+export const sayHi = (name) => {
+  name = 'Анатолий';
+  console.log(chalk.green('Привет,'), chalk.blue(name));
 };
 
-// 2. Функция sayText с chalk
-const sayText = (text) => {
+// 2. Функция sayText (Пример текста), чёрный фон и белый текст
+export const sayText = (text) => {
+  text = 'Пример текста';
   console.log(chalk.bgBlack.white(text));
 };
 
-// 3. Функция square
-const square = (num) => num * num;
+// 3. Функция square (Квадрат), любое число умноженное на себя
+export const square = (num) => {
+  return num * num;
+};
 
-// 4. Функция sum
-const sum = (a, b) => a + b;
+// 4. Функция sum (Сумма), которая принимает на вход два аргумента, а возвращает результат сложения
+export const sum = (a, b) => {
+  return a + b;
+};
 
-// 5. Функция power
-const power = (a, b) => a ** b;
-
-// Экспорт функций
-export { sayHi, sayText, square, sum, power };
+//Функцию power (Степени), которая принимает на вход два аргумента - число a и степень b, и возвращает число a, возведённое в степень b
+export const power = (a, b) => {
+  return a ** b;
+};
--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
-import { sayHi, sayText, square, sum, power } from './ functions.js';
-
-// Тестирование sayHi
-sayHi('Анатолий'); // Зелёное "Привет," и синее имя
-sayHi('Мария'); // Аналогично для другого имени
-
-// Тестирование sayText
-sayText('Важное сообщение'); // Белый текст на чёрном фоне
-sayText('Ещё один пример');
-
-// Тестирование математических функций
-console.log('5² =', square(5));
-console.log('2 + 3 =', sum(2, 3));
-console.log('2^10 =', power(2, 10));
+import { sayHi } from './ functions.js';
+import { sayText } from './ functions.js';
+import { square } from './ functions.js';
+import { sum } from './ functions.js';
+import { power } from './ functions.js';
+// Проверка решение задание
+sayHi(); // Привет, Анатолий
+sayText(); // Пример текста
+console.log('Квадрат 5:', square(5)); // Квадрат 5: 25
+console.log('Сумма 10+20:', sum(10, 20)); // Сумма 10+20: 30
+console.log('Степени 2^10:', power(2, 10)); // Степени 2^10: 1024
