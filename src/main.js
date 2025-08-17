d9c9f6abc6bedcc33e9f7692ea60299116b780ed
Саргис <oganyan.sargis20@mail.ru>
2025-08-17 20:24:06 +0300

    Строки:Длина строки

--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
-import { sayHi } from './ functions.js';
-import { sayText } from './ functions.js';
-import { square } from './ functions.js';
-import { sum } from './ functions.js';
-import { power } from './ functions.js';
-// Проверка решение задание
-sayHi(); // Привет, Анатолий
-sayText(); // Пример текста
-console.log('Квадрат 5:', square(5)); // Квадрат 5: 25
-console.log('Сумма 10+20:', sum(10, 20)); // Сумма 10+20: 30
-console.log('Степени 2^10:', power(2, 10)); // Степени 2^10: 1024
+import { len } from './strings.js';
+//Длина строки
+console.log(len('Привет!')); // 7
+console.log(len('Это очень длинная строка :)')); // 27
