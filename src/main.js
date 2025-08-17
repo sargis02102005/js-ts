4345a7f27557a6a98b44fbd6da40a8de09754935
Саргис <oganyan.sargis20@mail.ru>
2025-08-17 22:35:30 +0300

    Строки:Информация о строке

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-import { message } from './strings.js';
-//Шаблонные строки
-console.log(message('Алексей', '17'));
+import { logStringInfo } from './strings.js';
+
+logStringInfo('Это строка');
--- src/strings.js
+++ src/strings.js
@@ -1,7 +1,9 @@
-export const message = (name, age) => {
-  const message = `Добро пожаловать!
-Имя: ${name}
-Возраст: ${age}
-Надеемся, Вам понравится пользоваться нашим сервисом!`;
-  return message;
+import chalk from 'chalk';
+export const logStringInfo = (text) => {
+  //Первая буква жёлтым цветом
+  const first = chalk.yellow(text.slice(0, 1));
+  //Последняя буква зелёным цветом
+  const last = chalk.green(text.slice(text.length - 1));
+  //                        Её длина
+  console.log(`Символов: ${text.length}\nПревый: ${first}\nПоследний: ${last}`);
 };
