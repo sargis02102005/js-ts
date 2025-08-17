ad6a5592d0ad3d8dc7d1c538644c44b91e4c3f09
Саргис <oganyan.sargis20@mail.ru>
2025-08-17 21:18:46 +0300

    Строки:Первый и последний символ строки

--- src/strings.js
+++ src/strings.js
@@ -1,3 +1,11 @@
-//Функции с коротким телом могут писаться без фигурных скобок
-//.length - узнать длину строки, то есть кол-во символов в ней
-export const len = (str) => str.length;
+//Первый и последний символ строки записан в верхнем регистре
+export const func = (str) => {
+  //Любая первая буква записан в верхнем регистре
+  const stars = str.slice(0, 1).toUpperCase();
+  //Между ними записона в нижнем регистре
+  const middles = str.slice(1, str.length - 1).toLowerCase();
+  //Любая последная буква записан в верхнем регистре
+  const end = str.slice(str.length - 1).toUpperCase();
+
+  return stars + middles + end;
+};
