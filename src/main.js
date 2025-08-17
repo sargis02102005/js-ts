28a77c9f20f18f16d6b4a17b390aead16c7beaca
Саргис <oganyan.sargis20@mail.ru>
2025-08-17 22:06:27 +0300

    Строки:Шаблонные строки

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-import { isStringStartsWith } from './strings.js';
-
-console.log(isStringStartsWith('Ветеринар', 'вет')); // true
-console.log(isStringStartsWith('Молоко', 'мол')); // true
-console.log(isStringStartsWith('Карго Корги', 'КАРГО ')); // true
-
-console.log(isStringStartsWith('Карго Корги', 'кор ')); // false
-console.log(isStringStartsWith('Ковёр', 'кова ')); // false
+import { message } from './strings.js';
+//Шаблонные строки
+console.log(message('Алексей', '17'));
--- src/strings.js
+++ src/strings.js
@@ -1,5 +1,7 @@
-export const isStringStartsWith = (text, part) => {
-  // text.toLowerCase() и part.toLowerCase() приводят обе строки к нижнему регистру, чтобы сравнение было нечувствительным к регистру
-  //startsWith() проверяет, начинается ли строка text с подстроки part
-  return text.toLowerCase().startsWith(part.toLowerCase());
+export const message = (name, age) => {
+  const message = `Добро пожаловать!
+Имя: ${name}
+Возраст: ${age}
+Надеемся, Вам понравится пользоваться нашим сервисом!`;
+  return message;
 };
