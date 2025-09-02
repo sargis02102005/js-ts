c0fe9c55154d15214937953a02dedeac86f7d004
Саргис <oganyan.sargis20@mail.ru>
2025-09-02 18:13:58 +0300

    Циклы:Подходящие люди

--- src/loops.js
+++ src/loops.js
@@ -1,9 +1,22 @@
-export const revert = (str) => {
-  let reversed = '';
+export const filter = (people) => {
+  // Создаем объект для хранения результатов
+  const result = {
+    mans: [], // Массив для подходящих мужчин
+    women: [], // Массив для подходящих женщин
+  };
 
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
+  // Проходим по каждому человеку в массиве
+  for (const person of people) {
+    //Условия для мужчин: gender = 'm' И age > 18
+    if (person.gender === 'm' && person.age > 18) {
+      result.mans.push(person.name); // Добавляем имя в массив мужчин
+    }
+
+    //Условия для женщин: gender = 'f' И age > 20
+    if (person.gender === 'f' && person.age > 20) {
+      result.women.push(person.name); // Добавляем имя в массив женщин
+    }
   }
 
-  return reversed;
+  return result; // Возвращаем объект с результатами
 };
