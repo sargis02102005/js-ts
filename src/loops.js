bb4b24dbc4519a55c8c5013f3795b1890ccc86c6
Саргис <oganyan.sargis20@mail.ru>
2025-09-05 13:47:54 +0300

    Циклы:функция deduplicate с for i

--- src/loops.js
+++ src/loops.js
@@ -7,14 +7,11 @@
 // srt:uuunbbeliaaaaveeabbbblllllee // str.length = 28
 export const deduplicate = (str) => {
   let result = '';
-  let i = 0;
-  //i:0 < str.length:28
-  while (i < str.length) {
-    //str[0] = 'u', str[-1] = undefined
-    if (str[i] === str[i - 1]) {
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i - 1]) {
       result += str[i];
     }
-    i++;
   }
 
   return result;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import { deduplicate } from './loops.js';
 const result = deduplicate('uuunbbeliaaaaveeabbbblllllee');
+
 console.log(result); //uubaaaebbblllle
