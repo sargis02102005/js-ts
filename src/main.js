64e66e615a5887e644a962926f5c3e95f37634a9
Саргис <oganyan.sargis20@mail.ru>
2025-09-17 14:38:31 +0300

    Циклы:Сравнение объектов

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import { compareArrays } from './loops.js';
+import { compareObject } from './loops.js';
 
-console.log(compareArrays([], [])); // true
-console.log(compareArrays([true, null, 1, 'stop'], [true, null, 1, 'stop'])); // true
+console.log(compareObject({}, {})); // true
+console.log(compareObject({ age: 45 }, { age: 45 })); // true
+console.log(compareObject({ ready: true, photo: null }, { ready: true, photo: null })); // true
+console.log(compareObject({ a: 10, b: 20, c: 30 }, { c: 30, a: 10, b: 20 })); // true
 
-console.log(compareArrays([1, 2, 3], [1, 1, 3])); // false
-console.log(compareArrays([null], [])); // false
+console.log(compareObject({ name: 'max' }, { name: 'john' })); // false
+console.log(compareObject({ nick: 'maxon' }, {})); // false
+console.log(compareObject({ age: 45, name: 'a' }, { age: 45, name: 'b' })); // false
