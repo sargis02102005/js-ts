61344f7eb03206e00a634918162adb13fe405fae
Саргис <oganyan.sargis20@mail.ru>
2025-10-02 18:02:13 +0300

    Деструктуризация:Простой merge в новый объект

--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
-const arr = [5, 8, 1, 3];
-const shuffle = (src) => {
-  const j = Math.floor(Math.random() * src.length);
+const user = { name: 'Коля', age: 9 };
+const diplom = { degree: 'Магистр', university: 'МГУ', department: 'Кафедра информационных технологий' };
 
-  for (let i = 0; i < src.length; i++) {
-    [src[i], src[j]] = [src[j], src[i]];
-  }
-  return src;
-};
+// const mergeToNewObject = (odj1, odj2) => {
+//   return { ...odj1, ...odj2 };
+// };
 
-console.log(shuffle(arr));
+// const mergeToNewObject = () => {
+//   return { ...user, ...diplom };
+// };
+
+// const obj3 = mergeToNewObject(user, diplom);
+
+const obj3 = { ...user, ...diplom };
+
+console.log(obj3);
+console.log(khkkg);
+/*
+{
+  name: 'Коля',
+  age: 9,
+  degree: 'Магистр',
+  university: 'МГУ',
+  department: 'Кафедра информационных технологий'
+}
+ */
+M;
